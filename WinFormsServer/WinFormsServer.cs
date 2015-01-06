85b9d1602f725191674adf35ddefa1e40f84b3dd
mjg7822 <m78mjg@hotmail.com>
2015-01-06 16:28:08 +0900

    create

---
+++ WinFormsServer/WinFormsServer.cs
@@ -0,0 +1,210 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignalRChat
+{
+    /// <summary>
+    /// WinForms host for a SignalR server. The host can stop and start the SignalR
+    /// server, report errors when trying to start the server on a URI where a
+    /// server is already being hosted, and monitor when clients connect and disconnect. 
+    /// The hub used in this server is a simple echo service, and has the same 
+    /// functionality as the other hubs in the SignalR Getting Started tutorials.
+    /// </summary>
+    public partial class WinFormsServer : Form
+    {
+        private IDisposable SignalR { get; set; }
+        const string ServerURI = "http://localhost:8080";
+
+        internal WinFormsServer()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Calls the StartServer method with Task.Run to not
+        /// block the UI thread. 
+        /// </summary>
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            WriteToConsole("Starting server...");
+            ButtonStart.Enabled = false;
+            Task.Run(() => StartServer());
+        }
+
+        /// <summary>
+        /// Stops the server and closes the form. Restart functionality omitted
+        /// for clarity.
+        /// </summary>
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            //SignalR will be disposed in the FormClosing event
+            Close();
+        }
+
+        /// <summary>
+        /// Starts the server and checks for error thrown when another server is already 
+        /// running. This method is called asynchronously from Button_Start.
+        /// </summary>
+        private void StartServer()
+        {
+            try
+            {
+                SignalR = WebApp.Start(ServerURI);
+            }
+            catch (TargetInvocationException)
+            {
+                WriteToConsole("Server failed to start. A server is already running on " + ServerURI);
+                //Re-enable button to let user try to start server again
+                this.Invoke((Action)(() => ButtonStart.Enabled = true));
+                return;
+            }
+            this.Invoke((Action)(() => ButtonStop.Enabled = true));
+            WriteToConsole("Server started at " + ServerURI);
+        }
+        /// <summary>
+        /// This method adds a line to the RichTextBoxConsole control, using Invoke if used
+        /// from a SignalR hub thread rather than the UI thread.
+        /// </summary>
+        /// <param name="message"></param>
+        internal void WriteToConsole(String message)
+        {
+            if (RichTextBoxConsole.InvokeRequired)
+            {
+                this.Invoke((Action)(() =>
+                    WriteToConsole(message)
+                ));
+                return;
+            }
+            RichTextBoxConsole.AppendText(message + Environment.NewLine);
+        }
+
+        private void WinFormsServer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+            if (SignalR != null)
+            {
+                SignalR.Dispose();
+            }
+        }
+    }
+    /// <summary>
+    /// Used by OWIN's startup process. 
+    /// </summary>
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            var hubConfiguration = new HubConfiguration
+            {
+                // You can enable JSONP by uncommenting line below.
+                // JSONP requests are insecure but some older browsers (and some
+                // versions of IE) require JSONP to work cross domain
+                 EnableJSONP = true
+                 
+            };
+
+            app.MapSignalR(hubConfiguration);
+        }
+    }
+    public class Group
+    {
+        public string Name { get; set; }
+        public int MaxMember { get { return 2; } }
+        public bool IsAvailable { get; set; }
+        public string ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string GroupId { get; set; }
+    }
+    /// <summary>
+    /// Echoes messages sent using the Send message by calling the
+    /// addMessage method on the client. Also reports to the console
+    /// when clients connect and disconnect.
+    /// </summary>
+    public class MyHub : Hub
+    {
+        public static List<Group> groupList = new List<Group>();
+        public void Send(string name, string message,string groupName)
+        {
+            Clients.Group(groupName).addMessage(name, message);
+        }
+        public override Task OnConnected()
+        {
+            Program.MainForm.WriteToConsole("Client connected: " + Context.ConnectionId);
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected()
+        {
+            var client = groupList.Where(d => d.ClientId == Context.ConnectionId).FirstOrDefault();
+            var agent = groupList.Where(d => d.GroupId == Context.ConnectionId).FirstOrDefault();
+
+            if (client != null)
+            {
+                client.IsAvailable = true;
+                client.ClientId = String.Empty;
+                Groups.Remove(Context.ConnectionId, client.Name);
+                LeaveGroup(client.ClientName,client.Name);
+
+            }
+
+            if (agent != null)
+            {
+                groupList.Remove(agent);
+                Groups.Remove(Context.ConnectionId,agent.Name);
+            }
+
+            Program.MainForm.WriteToConsole("Client disconnected: " + Context.ConnectionId);
+            return base.OnDisconnected();
+        }
+        public void Join(string name, string groupName)
+        {
+
+            Clients.Group(groupName).joinMessage(name, String.Format("Join : {0}", name));
+        }
+
+        public void FindGroup(string name)
+        {
+            var group = groupList.Where(d => d.IsAvailable).FirstOrDefault();
+            if (group != null)
+            {
+                group.IsAvailable = false;
+                group.ClientId = Context.ConnectionId;
+                group.ClientName = name;
+                Groups.Add(Context.ConnectionId, group.Name);
+                Join(name,group.Name);
+            }
+            Clients.Caller.findedGroup(group == null ? "" : group.Name);
+        }
+
+        public Task JoinGroup(string name, string groupName)
+        {
+            var group = groupList.Where(d => d.Name.Equals(groupName, StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
+            if (group == null)
+            {
+                groupList.Add(new Group() { 
+                 IsAvailable =true,
+                  Name = groupName,
+                  ClientId = String.Empty,
+                 GroupId = Context.ConnectionId
+                });
+               
+            }
+            return Groups.Add(Context.ConnectionId, groupName);
+
+        }
+
+        public void LeaveGroup(string name,string groupName)
+        {
+            Clients.Group(groupName).leaveMessage(name);
+        }
+       
+    }
+}
---
+++ WindowsFormsClient/WinFormsClient.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WinFormsClient
+{
+    partial class WinFormsClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WinFormsClient));
+            this.ButtonSend = new System.Windows.Forms.Button();
+            this.TextBoxMessage = new System.Windows.Forms.TextBox();
+            this.RichTextBoxConsole = new System.Windows.Forms.RichTextBox();
+            this.ChatPanel = new System.Windows.Forms.Panel();
+            this.SignInPanel = new System.Windows.Forms.Panel();
+            this.StatusText = new System.Windows.Forms.Label();
+            this.SignInButton = new System.Windows.Forms.Button();
+            this.UserNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.ChatPanel.SuspendLayout();
+            this.SignInPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ButtonSend
+            // 
+            this.ButtonSend.Enabled = false;
+            this.ButtonSend.Location = new System.Drawing.Point(483, 9);
+            this.ButtonSend.Name = "ButtonSend";
+            this.ButtonSend.Size = new System.Drawing.Size(87, 21);
+            this.ButtonSend.TabIndex = 1;
+            this.ButtonSend.Text = "Send";
+            this.ButtonSend.UseVisualStyleBackColor = true;
+            this.ButtonSend.Click += new System.EventHandler(this.ButtonSend_Click);
+            // 
+            // TextBoxMessage
+            // 
+            this.TextBoxMessage.Location = new System.Drawing.Point(9, 11);
+            this.TextBoxMessage.Name = "TextBoxMessage";
+            this.TextBoxMessage.Size = new System.Drawing.Size(466, 21);
+            this.TextBoxMessage.TabIndex = 2;
+            // 
+            // RichTextBoxConsole
+            // 
+            this.RichTextBoxConsole.Location = new System.Drawing.Point(9, 35);
+            this.RichTextBoxConsole.Name = "RichTextBoxConsole";
+            this.RichTextBoxConsole.ReadOnly = true;
+            this.RichTextBoxConsole.Size = new System.Drawing.Size(560, 426);
+            this.RichTextBoxConsole.TabIndex = 3;
+            this.RichTextBoxConsole.Text = "";
+            this.RichTextBoxConsole.Resize += new System.EventHandler(this.Form_Resize);
+            // 
+            // ChatPanel
+            // 
+            this.ChatPanel.Controls.Add(this.RichTextBoxConsole);
+            this.ChatPanel.Controls.Add(this.TextBoxMessage);
+            this.ChatPanel.Controls.Add(this.ButtonSend);
+            this.ChatPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ChatPanel.Location = new System.Drawing.Point(0, 0);
+            this.ChatPanel.Name = "ChatPanel";
+            this.ChatPanel.Size = new System.Drawing.Size(584, 474);
+            this.ChatPanel.TabIndex = 4;
+            this.ChatPanel.Visible = false;
+            // 
+            // SignInPanel
+            // 
+            this.SignInPanel.Controls.Add(this.StatusText);
+            this.SignInPanel.Controls.Add(this.SignInButton);
+            this.SignInPanel.Controls.Add(this.UserNameTextBox);
+            this.SignInPanel.Controls.Add(this.label1);
+            this.SignInPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SignInPanel.Location = new System.Drawing.Point(0, 0);
+            this.SignInPanel.Name = "SignInPanel";
+            this.SignInPanel.Size = new System.Drawing.Size(584, 474);
+            this.SignInPanel.TabIndex = 4;
+            // 
+            // StatusText
+            // 
+            this.StatusText.Location = new System.Drawing.Point(14, 54);
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(556, 12);
+            this.StatusText.TabIndex = 6;
+            this.StatusText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.StatusText.Visible = false;
+            // 
+            // SignInButton
+            // 
+            this.SignInButton.Location = new System.Drawing.Point(483, 20);
+            this.SignInButton.Name = "SignInButton";
+            this.SignInButton.Size = new System.Drawing.Size(87, 21);
+            this.SignInButton.TabIndex = 5;
+            this.SignInButton.Text = "Sign In";
+            this.SignInButton.UseVisualStyleBackColor = true;
+            this.SignInButton.Click += new System.EventHandler(this.SignInButton_Click);
+            // 
+            // UserNameTextBox
+            // 
+            this.UserNameTextBox.Location = new System.Drawing.Point(8, 22);
+            this.UserNameTextBox.Name = "UserNameTextBox";
+            this.UserNameTextBox.Size = new System.Drawing.Size(467, 21);
+            this.UserNameTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter user name:";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            // 
+            // WinFormsClient
+            // 
+            this.AcceptButton = this.ButtonSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 474);
+            this.Controls.Add(this.ChatPanel);
+            this.Controls.Add(this.SignInPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(600, 512);
+            this.Name = "WinFormsClient";
+            this.Text = "WinForms SignalR Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.WinFormsClient_FormClosing);
+            this.Resize += new System.EventHandler(this.Form_Resize);
+            this.ChatPanel.ResumeLayout(false);
+            this.ChatPanel.PerformLayout();
+            this.SignInPanel.ResumeLayout(false);
+            this.SignInPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonSend;
+        private System.Windows.Forms.TextBox TextBoxMessage;
+        private System.Windows.Forms.RichTextBox RichTextBoxConsole;
+        private System.Windows.Forms.Panel ChatPanel;
+        private System.Windows.Forms.Panel SignInPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SignInButton;
+        private System.Windows.Forms.TextBox UserNameTextBox;
+        private System.Windows.Forms.Label StatusText;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+
+    }
+}
+
